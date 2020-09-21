b302a6b684ddeb7390004de98526e79963b4feb4
Arif Turmuji <arifturmuji316@gmail.com>
2020-09-21 22:27:19 +0700

    Create index.php

---
+++ hereuy/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		body {
+			background-color: black; 
+		}
+	</style>
+</head>
+<body>
+<?php for ($i=0; $i < 24; $i++) : ?> 
+	<h1 style="color: red; font-weight: bold; display: inline-block;">HAHAHAHAHAHAHAHAHA</h1>
+<?php endfor; ?>	
+</body>
+</html>
+
